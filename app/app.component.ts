697325567710c3724d97cee63a8f070675d7067d
PhilGebauer <gebauer.phil@gmail.com>
2017-05-10 16:30:57 -0500

    added user info and click handlers

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,56 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="jumbotron">
-      <h1>An Angular 2 Quickstart Boilerplate</h1>
-    </div>
+  <header>
+    <nav class="navbar navbar-inverse">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand"> Angular 2 App Practice </a>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+
+    <div class="row">
+      <div class="col-sm-4">
+          <div *ngIf="users">
+          <ul class="list-group user-list">
+            <li class="list-group-item"
+            *ngFor="let user of users"
+            (click)="selectUser(user)">
+            {{ user.name }} {{ user.username }}
+            </li>
+          </ul>
+          </div>
+      </div>
+      <div class="col-sm-8">
+          <div class="jumbotron">
+            <h1>An Angular 2 Quickstart Boilerplate</h1>
+          </div>
+      </div>
+      </div>
+  </main>
+
+
+    <footer class="text-center">
+    copyright 2017
+    </footer>
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
-export class AppComponent  {}
+
+export class AppComponent {
+  message = 'Hello!';
+  users = [
+    { id: 25, name: 'Chris', username: 'sevilayha' },
+    { id: 26, name: 'Nick', username: 'whatnicktweets' },
+    { id: 27, name: 'Holly', username: 'hollylawly' }
+  ];
+  activeUser;
+  selectUser (user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
+}
--- dist/app.component.js
+++ dist/app.component.js
@@ -9,13 +9,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello!';
+        this.users = [
+            { id: 25, name: 'Chris', username: 'sevilayha' },
+            { id: 26, name: 'Nick', username: 'whatnicktweets' },
+            { id: 27, name: 'Holly', username: 'hollylawly' }
+        ];
     }
+    AppComponent.prototype.selectUser = function (user) {
+        this.activeUser = user;
+        console.log(this.activeUser);
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n    <div class=\"jumbotron\">\n      <h1>An Angular 2 Quickstart Boilerplate</h1>\n    </div>\n  ",
+        template: "\n  <header>\n    <nav class=\"navbar navbar-inverse\">\n      <div class=\"navbar-header\">\n        <a href=\"/\" class=\"navbar-brand\"> Angular 2 App Practice </a>\n      </div>\n    </nav>\n  </header>\n\n  <main>\n\n    <div class=\"row\">\n      <div class=\"col-sm-4\">\n          <div *ngIf=\"users\">\n          <ul class=\"list-group user-list\">\n            <li class=\"list-group-item\"\n            *ngFor=\"let user of users\"\n            (click)=\"selectUser(user)\">\n            {{ user.name }} {{ user.username }}\n            </li>\n          </ul>\n          </div>\n      </div>\n      <div class=\"col-sm-8\">\n          <div class=\"jumbotron\">\n            <h1>An Angular 2 Quickstart Boilerplate</h1>\n          </div>\n      </div>\n      </div>\n  </main>\n\n\n    <footer class=\"text-center\">\n    copyright 2017\n    </footer>\n  ",
         styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
     })
 ], AppComponent);
